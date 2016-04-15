bdb1e5c08ca4f3122e15fd3ce982301baa84c8fc
Markus Hauck <markus1189@gmail.com>
2016-04-15 10:48:07 +0200

    Basic DSL

--- src/main/scala/de/codecentric/Algebra.scala
+++ src/main/scala/de/codecentric/Algebra.scala
@@ -1,21 +1,51 @@
 package de.codecentric
 
+import cats.Applicative
 import cats.free.{ Free, FreeApplicative }
-import java.time.LocalDate
+import cats.`~>`
+import cats._
+import cats.std.future._
+import cats.std.option._
+import java.time.{ LocalDate, LocalDateTime }
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-object Algebra {
+trait TimerDsl {
   sealed trait TimerF[A]
   type TimerM[A] = Free[TimerF, A]
   type TimerA[A] = FreeApplicative[TimerF, A]
 
-  trait Commands {
-    case object StartTimer extends TimerF[Unit]
-    case object StopTimer extends TimerF[Unit]
-    case object GetCurrentTimer extends TimerF[Option[TimerEntry]]
+  private case object StartTimer extends TimerF[Unit]
+  private case object StopTimer extends TimerF[Option[TimerEntry]]
+  private case object GetCurrentTimer extends TimerF[Option[RunningTimerEntry]]
+
+  // case class GetTimerEntries(day: LocalDate) extends TimerF[Vector[TimerEntry]]
+
+  object monadic {
+    def startTimer: TimerM[Unit] = Free.liftF(StartTimer)
+    def stopTimer: TimerM[Option[TimerEntry]] = Free.liftF(StopTimer)
+    def getCurrentTimer: TimerM[Option[RunningTimerEntry]] = Free.liftF(GetCurrentTimer)
+
+    // def getTimerEntries(day: LocalDate): TimerM[Vector[TimerEntry]] = Free.liftF(GetTimerEntries(day))
   }
 
-  trait Reporting {
-    case class GetTimerEntries(day: LocalDate) extends TimerF[Vector[TimerEntry]]
-    def getTimerEntries(day: LocalDate): TimerM[Vector[TimerEntry]] = Free.liftF(GetTimerEntries(day))
+  def run[A](p: TimerM[A]) = {
+    var isRunning = false
+    var currentStart: Option[LocalDateTime] = None
+    var currentStop: Option[LocalDateTime] = None
+
+    p.foldMap(new (TimerF ~> Id) {
+      def apply[B](fa: TimerF[B]): Id[B] = fa match {
+        case StartTimer =>
+          if (currentStart.isEmpty) currentStart = Some(LocalDateTime.now)
+        case StopTimer =>
+          currentStop = Some(LocalDateTime.now)
+          Applicative[Option].map2(currentStart, currentStop)(TimerEntry(_,_))
+        case GetCurrentTimer =>
+          currentStart.map(RunningTimerEntry)
+      }
+    })
   }
 }
+
+object TimerDsl extends TimerDsl
--- src/main/scala/de/codecentric/TimerEntry.scala
+++ src/main/scala/de/codecentric/TimerEntry.scala
@@ -5,3 +5,5 @@ import java.time.{ Duration, LocalDateTime }
 case class TimerEntry(start: LocalDateTime, stop: LocalDateTime) {
   def duration: Duration = Duration.between(start,stop)
 }
+
+case class RunningTimerEntry(start: LocalDateTime) extends AnyVal
