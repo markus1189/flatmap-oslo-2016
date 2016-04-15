770f7e7f73ff48ded1cadbc71bba646dde874d47
Markus Hauck <markus1189@gmail.com>
2016-04-15 13:53:53 +0200

    Add `doWork` and test DSL

--- src/main/scala/de/codecentric/Algebra.scala
+++ src/main/scala/de/codecentric/Algebra.scala
@@ -4,13 +4,10 @@ import cats.Applicative
 import cats.free.{ Free, FreeApplicative }
 import cats.`~>`
 import cats._
-import cats.std.future._
 import cats.std.option._
 import java.time.{ LocalDate, LocalDateTime }
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
-trait TimerDsl {
+trait TimerDsl extends Serializable {
   sealed trait TimerF[A]
   type TimerM[A] = Free[TimerF, A]
   type TimerA[A] = FreeApplicative[TimerF, A]
@@ -18,14 +15,15 @@ trait TimerDsl {
   private case object StartTimer extends TimerF[Unit]
   private case object StopTimer extends TimerF[Option[TimerEntry]]
   private case object GetCurrentTimer extends TimerF[Option[RunningTimerEntry]]
+  private case class DoWork(work: Eval[Any]) extends TimerF[Unit]
 
   // case class GetTimerEntries(day: LocalDate) extends TimerF[Vector[TimerEntry]]
 
-  object monadic {
+  object monadic extends Serializable {
     def startTimer: TimerM[Unit] = Free.liftF(StartTimer)
     def stopTimer: TimerM[Option[TimerEntry]] = Free.liftF(StopTimer)
     def getCurrentTimer: TimerM[Option[RunningTimerEntry]] = Free.liftF(GetCurrentTimer)
-
+    def doWork[A](work: => A): TimerM[Unit] = Free.liftF(DoWork(Eval.later(work)))
     // def getTimerEntries(day: LocalDate): TimerM[Vector[TimerEntry]] = Free.liftF(GetTimerEntries(day))
   }
 
@@ -39,9 +37,15 @@ trait TimerDsl {
           if (currentStart.isEmpty) currentStart = Some(LocalDateTime.now)
         case StopTimer =>
           currentStop = Some(LocalDateTime.now)
-          Applicative[Option].map2(currentStart, currentStop)(TimerEntry(_,_))
+          val r = Applicative[Option].map2(currentStart, currentStop)(TimerEntry(_,_))
+          currentStart = None
+          currentStop = None
+          r
         case GetCurrentTimer =>
           currentStart.map(RunningTimerEntry)
+        case DoWork(work) =>
+          work.value
+          ()
       }
     })
   }
--- src/test/scala/de/codecentric/TimerDslSpec.scala
+++ src/test/scala/de/codecentric/TimerDslSpec.scala
@@ -1,16 +1,54 @@
 package de.codecentric
 
+import cats.{ Applicative, Monad }
 import java.time.{ Duration, LocalDateTime }
 import org.scalatest._
+import cats.std.list._
 
-class TimerDslSpec extends FunSpec with Matchers {
+class TimerDslSpec extends FunSpec with Matchers with Inspectors {
   describe("TimerDsl") {
+    import TimerDsl._
+    import TimerDsl.monadic._
+
     describe("stopping the current timer") {
       it("should return None if no timer is running") {
-        import TimerDsl.monadic._
-
         val timer: Option[TimerEntry] = TimerDsl.locally(stopTimer)
+
         timer should be(None)
+      }
+
+      it("should return the finished timer") {
+        val timer: Option[TimerEntry] = TimerDsl.locally {
+          for {
+            _ <- startTimer
+            t <- stopTimer
+          } yield t
+        }
+
+        timer shouldBe defined
+      }
+
+      it("should return the finished timer repeatedly") {
+        val n: Int = 3
+
+        def startStop[A]: TimerM[Option[TimerEntry]] = for {
+          _ <- startTimer
+          _ = Thread.sleep(10)
+          t <- stopTimer
+        } yield t
+
+        val entries: TimerM[List[Option[TimerEntry]]] =
+          Applicative[TimerM].sequence(List.fill(n)(startStop))
+
+        val results = TimerDsl.locally(entries)
+
+        val starts = results.flatten.map(_.start)
+        println(starts)
+        val stops = results.flatten.map(_.stop)
+
+        results.flatten should have length(n.toLong)
+        starts.distinct should have length(n.toLong)
+        stops.distinct should have length(n.toLong)
       }
     }
   }
