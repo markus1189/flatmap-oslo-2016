c4a4a203cf2510fc087f846f298c38dbdc55773a
Markus Hauck <markus1189@gmail.com>
2016-04-17 17:01:55 +0200

    Start work on embedding

--- src/main/scala/de/codecentric/Algebra.scala
+++ src/main/scala/de/codecentric/Algebra.scala
@@ -4,6 +4,7 @@ import cats.arrow._
 import cats._
 import cats.`~>`
 import cats.data.State
+import cats.data.Coproduct
 import cats.free.{ Free, FreeApplicative }
 import cats.std.option._
 import java.time.{ LocalDate, LocalDateTime }
@@ -12,11 +13,14 @@ trait TimerDsl extends Serializable {
   sealed trait TimerF[A]
   type TimerM[A] = Free[TimerF, A]
   type TimerA[A] = FreeApplicative[TimerF, A]
+  type Timer[A] = Free[Coproduct[TimerF,Embed[TimerA,?],?],A]
 
   private case object StartTimer extends TimerF[Unit]
   private case object StopTimer extends TimerF[Option[TimerEntry]]
   private case object GetCurrentTimer extends TimerF[Option[RunningTimerEntry]]
   private case class DoWork(work: Eval[Any]) extends TimerF[Unit]
+
+  case class Embed[F[_],A](value: F[A])
   // case class GetTimerEntries(day: LocalDate) extends TimerF[Vector[TimerEntry]]
 
   object monadic extends Serializable {
@@ -27,33 +31,47 @@ trait TimerDsl extends Serializable {
     // def getTimerEntries(day: LocalDate): TimerM[Vector[TimerEntry]] = Free.liftF(GetTimerEntries(day))
   }
 
+  object applicative extends Serializable {
+    def startTimer: TimerA[Unit] = FreeApplicative.lift(StartTimer)
+    def stopTimer: TimerA[Option[TimerEntry]] = FreeApplicative.lift(StopTimer)
+    def getCurrentTimer: TimerA[Option[RunningTimerEntry]] = FreeApplicative.lift(GetCurrentTimer)
+    def doWork[A](work: => A): TimerA[Unit] = FreeApplicative.lift(DoWork(Eval.later(work)))
+  }
+
+  val StateOption = Applicative[State[S,?]].compose[Option]
+  type S = (Option[LocalDateTime],Option[LocalDateTime])
+  def currentStart: State[S,Option[LocalDateTime]] = State.inspect(_._1)
+  def currentStop: State[S,Option[LocalDateTime]] = State.inspect(_._2)
+
+  val interpTimerF: TimerF ~> State[S,?] = new (TimerF ~> State[S,?]) {
+    def apply[B](fa: TimerF[B]): State[S,B] = fa match {
+      case StartTimer =>
+        Monad[State[S,?]].ifM(currentStart.map(_.isEmpty))(
+          State.modify[S] { case (start,stop) => (Some(LocalDateTime.now),stop)},
+          State.pure(()))
+
+      case StopTimer => for {
+        _ <- State.modify[S] { startStop => (startStop._1,Some(LocalDateTime.now)) }
+        r <- StateOption.map2(currentStart,currentStop)(TimerEntry(_,_))
+        _ <- State.set((None: Option[LocalDateTime],None: Option[LocalDateTime]))
+      } yield r
+
+      case GetCurrentTimer =>
+        StateOption.map(currentStart)(RunningTimerEntry(_))
+
+      case DoWork(work) =>
+        work.value
+        State.pure(())
+    }
+  }
+
+  val interpEmbed: Embed[TimerA,?] ~> State[S,?] = new (Embed[TimerA,?] ~> State[S,?]) {
+    def apply[B](fa: Embed[TimerA,B]): State[S,B] =
+      fa.value.monad.foldMap[State[S,?]](interpTimerF)
+  }
+
   def purely[A](p: TimerM[A]): A = {
-    type S = (Option[LocalDateTime],Option[LocalDateTime])
-
-    def currentStart: State[S,Option[LocalDateTime]] = State.inspect(_._1)
-    def currentStop: State[S,Option[LocalDateTime]] = State.inspect(_._2)
-
-    p.foldMap[State[S,?]](new (TimerF ~> State[S,?]) {
-      def apply[B](fa: TimerF[B]): State[S,B] = fa match {
-        case StartTimer =>
-          Monad[State[S,?]].ifM(currentStart.map(_.isEmpty))(
-            State.modify[S] { case (start,stop) => (Some(LocalDateTime.now),stop)},
-            State.pure(()))
-
-        case StopTimer => for {
-          _ <- State.modify[S] { startStop => (startStop._1,Some(LocalDateTime.now)) }
-          r <- Applicative[State[S,?]].compose[Option].map2(currentStart,currentStop)(TimerEntry(_,_))
-          _ <- State.set((None: Option[LocalDateTime],None: Option[LocalDateTime]))
-        } yield r
-
-        case GetCurrentTimer =>
-          Functor[State[S,?]].compose[Option].map(currentStart)(RunningTimerEntry(_))
-          
-        case DoWork(work) =>
-          work.value
-          State.pure(())
-      }
-    }).runA(None,None).value
+    p.foldMap[State[S,?]](interpTimerF).runA((None,None)).value
   }
 }
 
